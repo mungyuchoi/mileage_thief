6230c162ea04de4ee51e97ad48f5d1f03a3365fa
DESKTOP-MQ <skylife927@gmail.com>
2023-02-10 18:10:02 +0900

    appbar, toogle구현 다음 commit으로는 widget별로 구분해야함

---
+++ lib/screen/search_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _SearchScreenState();
+}
+
+const double width = 300.0;
+const double height = 60.0;
+const double loginAlign = -1;
+const double signInAlign = 1;
+const Color selectedColor = Colors.white;
+const Color normalColor = Colors.white;
+
+class _SearchScreenState extends State<SearchScreen> {
+  double xAlign = 5.0;
+  Color loginColor = Colors.black;
+  Color signInColor = Colors.black;
+
+  @override
+  void initState() {
+    super.initState();
+    xAlign = loginAlign;
+    loginColor = selectedColor;
+    signInColor = normalColor;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '마일리지 도둑',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: Image.asset(
+          'asset/img/airplane.png',
+          scale: 2,
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: Center(
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(
+              Radius.circular(50.0),
+            ),
+          ),
+          child: Stack(
+            children: [
+              AnimatedAlign(
+                alignment: Alignment(xAlign, 0),
+                duration: Duration(milliseconds: 300),
+                child: Container(
+                  width: width * 0.5,
+                  height: height,
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(50.0),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    xAlign = loginAlign;
+                    loginColor = selectedColor;
+                    signInColor = normalColor;
+                  });
+                },
+                child: Align(
+                  alignment: Alignment(-1, 0),
+                  child: Container(
+                    width: width * 0.5,
+                    color: Colors.transparent,
+                    alignment: Alignment.center,
+                    child: Text(
+                      '왕복',
+                      style: TextStyle(
+                        color: loginColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    xAlign = signInAlign;
+                    signInColor = selectedColor;
+
+                    loginColor = normalColor;
+                  });
+                },
+                child: Align(
+                  alignment: Alignment(1, 0),
+                  child: Container(
+                    width: width * 0.5,
+                    color: Colors.transparent,
+                    alignment: Alignment.center,
+                    child: Text(
+                      '편도',
+                      style: TextStyle(
+                        color: signInColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
