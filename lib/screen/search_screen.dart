fea21eb4979a65a051bed4c0f25c04f08402ce3d
DESKTOP-MQ <skylife927@gmail.com>
2023-02-13 12:38:24 +0900

    클래스 검색박수 추가

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 
+import '../custom/CustomDropdownButton2.dart';
+
 class SearchScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SearchScreenState();
@@ -151,40 +153,159 @@ class AirportScreen extends StatefulWidget {
 }
 
 class _AirportScreenState extends State<AirportScreen> {
+  final List<String> dateItems = [
+    "전체",
+    "1박2일",
+    "2박3일",
+    "3박4일",
+    "4박5일",
+    "5박6일",
+    "6박7일",
+    "7박8일",
+    "8박9일",
+    "9박10일",
+    "10박11일",
+    "11박12일",
+    "12박13일",
+    "13박14일",
+    "14박15일",
+    "15박16일",
+    "16박17일",
+    "17박18일",
+    "18박19일",
+    "19박20일",
+    "20박21일",
+    "21박22일",
+    "22박23일",
+    "23박24일",
+    "24박25일",
+    "25박26일",
+    "26박27일",
+    "27박28일",
+    "28박29일",
+    "29박30일",
+  ];
+  final List<String> classItems = ["전체", "이코노미", "비즈니스", "퍼스트"];
+  final List<String> airportItems = ["서울/인천\nICN", "뉴욕/존F케네디\nJFK"];
+  String? dateSelectedValue;
+  String? classSelectedValue;
+  String? departureSelectedValue;
+  String? arrivalSelectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(15),
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: ListView(
-          padding: EdgeInsets.all(4),
+          padding: const EdgeInsets.all(4),
           children: <Widget>[
-            Divider(),
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            const Padding(padding: EdgeInsets.all(8)),
             Row(
               children: [
                 Expanded(
-                    child: DropdownSearch<String>(
-                  items: const ["서울/인천\nICN", "뉴욕/존F케네디\nJFK"],
-                )),
-                Padding(padding: EdgeInsets.all(4)),
-                Icon(
+                  child: CustomDropdownButton2(
+                    hint: '어디서 가나요?',
+                    dropdownItems: airportItems,
+                    hintAlignment: Alignment.center,
+                    value: departureSelectedValue,
+                    onChanged: (value) {
+                      setState(() {
+                        departureSelectedValue = value;
+                      });
+                    },
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.all(4)),
+                const Icon(
                   Icons.double_arrow_rounded,
                 ),
+                const Padding(padding: EdgeInsets.all(4)),
                 Expanded(
-                    child: DropdownSearch<String>(
-                  items: const ["서울/인천\nICN", "뉴욕/존F케네디\nJFK"],
-                )),
+                  child: CustomDropdownButton2(
+                    hint: '어디로 가나요?',
+                    hintAlignment: Alignment.center,
+                    dropdownItems: airportItems,
+                    value: arrivalSelectedValue,
+                    onChanged: (value) {
+                      setState(() {
+                        arrivalSelectedValue = value;
+                      });
+                    },
+                  ),
+                ),
               ],
             ),
-            Padding(padding: EdgeInsets.all(8)),
-            Divider(),
+            const Padding(padding: EdgeInsets.all(8)),
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            const Padding(padding: EdgeInsets.all(8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    const Text('클래스'),
+                    const Padding(padding: EdgeInsets.all(4)),
+                    CustomDropdownButton2(
+                      buttonWidth: 90,
+                      dropdownWidth: 100,
+                      valueAlignment: Alignment.center,
+                      hint: '전체',
+                      dropdownItems: classItems,
+                      value: classSelectedValue,
+                      onChanged: (value) {
+                        setState(() {
+                          classSelectedValue = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('검색박수'),
+                    const Padding(padding: EdgeInsets.all(4)),
+                    CustomDropdownButton2(
+                      buttonWidth: 100,
+                      dropdownWidth: 100,
+                      valueAlignment: Alignment.center,
+                      hint: '전체',
+                      dropdownItems: dateItems,
+                      value: dateSelectedValue,
+                      onChanged: (value) {
+                        setState(() {
+                          dateSelectedValue = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(8)),
+            const Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            const Padding(padding: EdgeInsets.all(8)),
             ElevatedButton(
               onPressed: () {},
-              child: Text("검색하기", style: TextStyle(fontSize: 18),),
               style: TextButton.styleFrom(
-                  primary: Colors.white, backgroundColor: Colors.black54,
-              minimumSize: Size.fromHeight(56.0)),
+                  primary: Colors.white,
+                  backgroundColor: Colors.black54,
+                  minimumSize: const Size.fromHeight(56.0)),
+              child: const Text(
+                "검색하기",
+                style: TextStyle(fontSize: 18),
+              ),
             ),
           ],
         ));
