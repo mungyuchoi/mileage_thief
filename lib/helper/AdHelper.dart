1e73456d7eae4ed25f18ea9cb5e702e6d4f3c153
mungyu <skylife927@gmail.com>
2023-03-09 23:07:18 +0900

    ios appid update

--- lib/helper/AdHelper.dart
+++ lib/helper/AdHelper.dart
@@ -7,7 +7,7 @@ class AdHelper {
       if (Platform.isAndroid) {
         return 'ca-app-pub-8549606613390169/3659488883';
       } else if (Platform.isIOS) {
-        return 'ca-app-pub-3940256099942544/2934735716';
+        return 'ca-app-pub-8549606613390169/9487079377';
       } else {
         throw new UnsupportedError('Unsupported platform');
       }
