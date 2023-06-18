4c885b430c7e07875dc28eef1176a323131a2d05
DESKTOP-MQ <skylife927@gmail.com>
2023-06-18 23:28:30 +0900

    1. 1.0.23 버전업데이트 2. 땅콩 구매 가능 버튼 활성화

---
+++ lib/screen/setting/setting_purchase.dart
@@ -0,0 +1,393 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:in_app_purchase_storekit/in_app_purchase_storekit.dart';
+import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
+import 'package:mileage_thief/screen/setting/consumable_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const List<String> _kProductIds = <String>[
+  'pinut20', 'pinut20random', 'pinut50', 'pinut50random'
+];
+class SettingPurchase extends StatefulWidget {
+  const SettingPurchase({super.key});
+
+  @override
+  State<StatefulWidget> createState() => SettingPurchaseState();
+}
+class SettingPurchaseState extends State<SettingPurchase> {
+  final InAppPurchase _inAppPurchase = InAppPurchase.instance;
+  late StreamSubscription<List<PurchaseDetails>> _subscription;
+  List<String> _notFoundIds = <String>[];
+  List<ProductDetails> _products = <ProductDetails>[];
+  List<PurchaseDetails> _purchases = <PurchaseDetails>[];
+  List<String> _consumables = <String>[];
+  bool _isAvailable = false;
+  bool _purchasePending = false;
+  bool _loading = true;
+  String? _queryProductError;
+
+  @override
+  void initState() {
+    final Stream<List<PurchaseDetails>> purchaseUpdated =
+        _inAppPurchase.purchaseStream;
+    _subscription =
+        purchaseUpdated.listen((List<PurchaseDetails> purchaseDetailsList) {
+          _listenToPurchaseUpdated(purchaseDetailsList);
+        }, onDone: () {
+          _subscription.cancel();
+        }, onError: (Object error) {
+          // handle error here.
+        });
+    initStoreInfo();
+    super.initState();
+  }
+
+  Future<void> initStoreInfo() async {
+    final bool isAvailable = await _inAppPurchase.isAvailable();
+    if (!isAvailable) {
+      setState(() {
+        _isAvailable = isAvailable;
+        _products = <ProductDetails>[];
+        _purchases = <PurchaseDetails>[];
+        _notFoundIds = <String>[];
+        _consumables = <String>[];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iosPlatformAddition =
+      _inAppPurchase
+          .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iosPlatformAddition.setDelegate(ExamplePaymentQueueDelegate());
+    }
+
+    final ProductDetailsResponse productDetailResponse =
+    await _inAppPurchase.queryProductDetails(_kProductIds.toSet());
+    if (productDetailResponse.error != null) {
+      setState(() {
+        _queryProductError = productDetailResponse.error!.message;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _purchases = <PurchaseDetails>[];
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _consumables = <String>[];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (productDetailResponse.productDetails.isEmpty) {
+      setState(() {
+        _queryProductError = null;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _purchases = <PurchaseDetails>[];
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _consumables = <String>[];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    final List<String> consumables = await ConsumableStore.load();
+    setState(() {
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _notFoundIds = productDetailResponse.notFoundIDs;
+      _consumables = consumables;
+      _purchasePending = false;
+      _loading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iosPlatformAddition =
+      _inAppPurchase
+          .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      iosPlatformAddition.setDelegate(null);
+    }
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> stack = <Widget>[];
+    if (_queryProductError == null) {
+      stack.add(
+        ListView(
+          children: <Widget>[
+            _buildProductList(),
+          ],
+        ),
+      );
+    } else {
+      stack.add(Center(
+        child: Text(_queryProductError!),
+      ));
+    }
+    if (_purchasePending) {
+      stack.add(
+        // TODO(goderbauer): Make this const when that's available on stable.
+        // ignore: prefer_const_constructors
+        Stack(
+          children: const <Widget>[
+            Opacity(
+              opacity: 0.3,
+              child: ModalBarrier(dismissible: false, color: Colors.grey),
+            ),
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: const Text('마일리지 도둑 땅콩 구매', style: TextStyle(color: Colors.white)),
+          leading: IconButton(
+            color: Colors.white,
+            icon: const Icon(Icons.arrow_back_ios_sharp),
+            iconSize: 20,
+            onPressed: () => Navigator.pop(context),
+          ),
+        ),
+        body: Stack(
+          children: stack,
+        ),
+      ),
+    );
+  }
+
+  Card _buildProductList() {
+    if (_loading) {
+      return const Card(
+          child: ListTile(
+              leading: CircularProgressIndicator(),
+              title: Text('로딩중..')));
+    }
+    if (!_isAvailable) {
+      return const Card();
+    }
+    const ListTile productHeader = ListTile(title: Text('땅콩 구매'));
+    final List<ListTile> productList = <ListTile>[];
+    if (_notFoundIds.isNotEmpty) {
+      productList.add(ListTile(
+          title: Text('[${_notFoundIds.join(", ")}] not found',
+              style: TextStyle(color: ThemeData.light().colorScheme.error)),
+          subtitle: const Text(
+              'This app needs special configuration to run. Please see example/README.md for instructions.')));
+    }
+
+    // This loading previous purchases code is just a demo. Please do not use this as it is.
+    // In your app you should always verify the purchase data using the `verificationData` inside the [PurchaseDetails] object before trusting it.
+    // We recommend that you use your own server to verify the purchase data.
+    final Map<String, PurchaseDetails> purchases =
+    Map<String, PurchaseDetails>.fromEntries(
+        _purchases.map((PurchaseDetails purchase) {
+          if (purchase.pendingCompletePurchase) {
+            _inAppPurchase.completePurchase(purchase);
+          }
+          return MapEntry<String, PurchaseDetails>(purchase.productID, purchase);
+        }));
+    productList.addAll(_products.map(
+          (ProductDetails productDetails) {
+        final PurchaseDetails? previousPurchase = purchases[productDetails.id];
+        return ListTile(
+          title: Text(
+            productDetails.title,
+          ),
+          subtitle: Text(
+            productDetails.description,
+          ),
+          trailing: previousPurchase != null && Platform.isIOS
+              ? IconButton(
+              onPressed: () => confirmPriceChange(context),
+              icon: const Icon(Icons.upgrade))
+              : TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.black54,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              late PurchaseParam purchaseParam;
+
+              if (Platform.isAndroid) {
+                purchaseParam = PurchaseParam(
+                  productDetails: productDetails,
+                );
+              }
+              _inAppPurchase.buyConsumable(
+                  purchaseParam: purchaseParam,
+                  autoConsume: true);
+            },
+            child: Text(productDetails.price),
+          ),
+        );
+      },
+    ));
+
+    return Card(
+        child: Column(
+            children: <Widget>[productHeader, const Divider()] + productList));
+  }
+
+  Future<void> consume(String id) async {
+    await ConsumableStore.consume(id);
+    final List<String> consumables = await ConsumableStore.load();
+    setState(() {
+      _consumables = consumables;
+    });
+  }
+
+  void showPendingUI() {
+    setState(() {
+      _purchasePending = true;
+    });
+  }
+
+  Future<void> deliverProduct(PurchaseDetails purchaseDetails) async {
+    int pinut = 0;
+    Random random = Random();
+    switch (purchaseDetails.productID) {
+      case "pinut20":
+        pinut = 20;
+        break;
+      case "pinut20random":
+        pinut = random.nextInt(21) + 10;
+        break;
+      case "pinut50":
+        pinut = 50;
+        break;
+      case "pinut50random":
+        pinut = random.nextInt(31) + 40;
+        break;
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int count = (prefs.getInt('counter') ?? 0) + pinut;
+    prefs.setInt('counter', count);
+    Fluttertoast.showToast(
+      msg: "땅콩 $pinut개를 얻었습니다.",
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 5,
+      backgroundColor: Colors.black38,
+      fontSize: 20,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_LONG,
+    );
+    setState(() {
+          _purchasePending = false;
+    });
+    // IMPORTANT!! Always verify purchase details before delivering the product.
+    // if (purchaseDetails.productID == _kConsumableId) {
+    //   await ConsumableStore.save(purchaseDetails.purchaseID!);
+    //   final List<String> consumables = await ConsumableStore.load();
+    //   setState(() {
+    //     _purchasePending = false;
+    //     _consumables = consumables;
+    //   });
+    // } else {
+    //   setState(() {
+    //     _purchases.add(purchaseDetails);
+    //     _purchasePending = false;
+    //   });
+    // }
+  }
+
+  void handleError(IAPError error) {
+    setState(() {
+      _purchasePending = false;
+    });
+  }
+
+  Future<bool> _verifyPurchase(PurchaseDetails purchaseDetails) {
+    // IMPORTANT!! Always verify a purchase before delivering the product.
+    // For the purpose of an example, we directly return true.
+    return Future<bool>.value(true);
+  }
+
+  void _handleInvalidPurchase(PurchaseDetails purchaseDetails) {
+    // handle invalid purchase here if  _verifyPurchase` failed.
+  }
+
+  Future<void> _listenToPurchaseUpdated(
+      List<PurchaseDetails> purchaseDetailsList) async {
+    for (final PurchaseDetails purchaseDetails in purchaseDetailsList) {
+      if (purchaseDetails.status == PurchaseStatus.pending) {
+        showPendingUI();
+      } else {
+        if (purchaseDetails.status == PurchaseStatus.error) {
+          handleError(purchaseDetails.error!);
+        } else if (purchaseDetails.status == PurchaseStatus.purchased ||
+            purchaseDetails.status == PurchaseStatus.restored) {
+          final bool valid = await _verifyPurchase(purchaseDetails);
+          if (valid) {
+            unawaited(deliverProduct(purchaseDetails));
+          } else {
+            _handleInvalidPurchase(purchaseDetails);
+            return;
+          }
+        }
+        // if (Platform.isAndroid) {
+        //   if (!_kAutoConsume && purchaseDetails.productID == _kConsumableId) {
+        //     final InAppPurchaseAndroidPlatformAddition androidAddition =
+        //     _inAppPurchase.getPlatformAddition<
+        //         InAppPurchaseAndroidPlatformAddition>();
+        //     await androidAddition.consumePurchase(purchaseDetails);
+        //   }
+        // }
+        if (purchaseDetails.pendingCompletePurchase) {
+          await _inAppPurchase.completePurchase(purchaseDetails);
+        }
+      }
+    }
+  }
+
+  Future<void> confirmPriceChange(BuildContext context) async {
+    // Price changes for Android are not handled by the application, but are
+    // instead handled by the Play Store. See
+    // https://developer.android.com/google/play/billing/price-changes for more
+    // information on price changes on Android.
+    if (Platform.isIOS) {
+      final InAppPurchaseStoreKitPlatformAddition iapStoreKitPlatformAddition =
+      _inAppPurchase
+          .getPlatformAddition<InAppPurchaseStoreKitPlatformAddition>();
+      await iapStoreKitPlatformAddition.showPriceConsentIfNeeded();
+    }
+  }
+}
+
+/// Example implementation of the
+/// [`SKPaymentQueueDelegate`](https://developer.apple.com/documentation/storekit/skpaymentqueuedelegate?language=objc).
+///
+/// The payment queue delegate can be implementated to provide information
+/// needed to complete transactions.
+class ExamplePaymentQueueDelegate implements SKPaymentQueueDelegateWrapper {
+  @override
+  bool shouldContinueTransaction(
+      SKPaymentTransactionWrapper transaction, SKStorefrontWrapper storefront) {
+    return true;
+  }
+
+  @override
+  bool shouldShowPriceConsent() {
+    return false;
+  }
+}
